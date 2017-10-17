83166bd4cf834cafdcd8440661902ec52f99a7a2
vlado <vlado@consultcommerce.com>
2017-10-17 09:17:48 +0300

    g

---
+++ gw2 Investment Tool/Controls/GoldValueControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gw2_Investment_Tool.Controls
+{
+	public partial class GoldValueControl : UserControl
+	{
+		public GoldValueControl()
+		{
+			InitializeComponent();
+
+		}
+
+		public void BindValues(int gold, int silver, int copper)
+		{
+			labelCopper.Text = copper.ToString();
+			labelGold.Text = gold.ToString();
+			labelSilver.Text = silver.ToString();
+		}
+
+
+	}
+}
